7662e7d740f7a89d1d169ad63a9a922729b7c374
alxx <alexanderkoh@funny-man.local>
2024-07-16 08:42:03 +0800

    reverting my mistakes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Analytics } from '@vercel/analytics/react';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
   document.getElementById("root")
 );
